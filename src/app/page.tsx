f7372c4f19385e7f816fd4f7b1fa5b71366251cf
onemineus <sangeetbanerjee777@gmail.com>
2023-10-02 22:33:16 +0530

    removed three

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -34,7 +34,7 @@ const Home = () => {
         <ReverseSection />
         <BrandSection />
         <A />
-        <Three />
+        {/* <Three /> */}
         <HeroSection setVariant={variantCB} />
         <Footer />
       </Container>
