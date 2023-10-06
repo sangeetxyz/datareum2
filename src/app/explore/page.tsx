11d99fc191a7c9c3aa10e99bcb872a38fa0ba573
onemineus <sangeetbanerjee777@gmail.com>
2023-10-06 09:01:39 +0530

    started exploring

--- src/app/explore/page.tsx
+++ src/app/explore/page.tsx
@@ -4,10 +4,10 @@ import React from "react";
 
 const Explore = () => {
   return (
-    <div className="min-h-screen bg-gradient-to-br from-stone-950 via-stone-900 to-stone-950">
+    <div className="min-h-screen flex justify-center bg-gradient-to-b from-stone-900  to-stone-950">
       <DashHeader />
-      <div className="h-screen w-full pt-20">
-        <div className="h-full">
+      <div className="h-screen w-full pt-20 max-w-6xl">
+        <div className="bg-red-400 w-full h-96">
           
         </div>
       </div>
