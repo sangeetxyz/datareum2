57981e0daf3f6c7b93c99622cd9497df7c97c25c
onemineus <sangeetbanerjee777@gmail.com>
2023-10-07 14:48:13 +0530

    added profile refresh

--- src/app/api/dev/patients/route.ts
+++ src/app/api/dev/patients/route.ts
@@ -13,7 +13,7 @@ export async function GET(request: Request, response: Response) {
     const authHeader = request.headers.get("authorization");
     if (authHeader) {
       const token = authHeader?.split(" ")[1];
-      if (token === process.env.NEXT_PUBLIC_API_SECRET) {
+      if (token !== process.env.NEXT_PUBLIC_API_SECRET) {
         return NextResponse.json({ error: "Unauthorized" });
       } else {
         const patientData = await prisma.patient.findMany();
@@ -39,7 +39,7 @@ export async function POST(request: Request, response: Response) {
     const authHeader = request.headers.get("authorization");
     if (authHeader) {
       const token = authHeader?.split(" ")[1];
-      if (token === process.env.NEXT_PUBLIC_API_SECRET) {
+      if (token !== process.env.NEXT_PUBLIC_API_SECRET) {
         return NextResponse.json({ error: "Unauthorized" });
       } else {
         const body = await request.json();
