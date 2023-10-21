8e7a99b8471ff5cb1eae7b2ec62064b14bd37b98
onemineus <sangeetbanerjee777@gmail.com>
2023-10-21 11:14:13 +0530

    added dark testing

--- src/app/dark/page.tsx
+++ src/app/dark/page.tsx
@@ -5,25 +5,25 @@ import { useAtom } from "jotai";
 import React, { useState } from "react";
 
 const Dark = () => {
-  //   const [isLite, setIsLite] = useAtom(isDark);
-  const val = useMotionValue(1);
+  const [isLite, setIsLite] = useAtom(isDark);
+  //   const [scale, setScale] = useState(1);
   return (
-    <div className="flex h-screen items-center justify-center bg-black">
-      <AnimatePresence>
-        <motion.div
-          key={"1"}
-          onClick={() => {
-            val.set(10);
-          }}
-          initial={{ backgroundColor: "white" }}
-          animate={{ backgroundColor: "black" }}
-          transition={{ duration: 39, ease: "easeInOut" }}
-          className="cursor-pointer text-9xl"
-        >
-          dark
-        </motion.div>
-      </AnimatePresence>
-    </div>
+    <motion.div
+      animate={{ backgroundColor: `${!isLite ? "#fff" : "#000"}` }}
+      transition={{ duration: 2, ease: "easeIn" }}
+      className="flex h-screen items-center justify-center bg-black"
+    >
+      <motion.div
+        animate={{ color: `${isLite ? "#fff" : "#000"}` }}
+        transition={{ duration: 2, ease: "easeIn" }}
+        onClick={() => {
+          setIsLite(!isLite);
+        }}
+        className="cursor-pointer text-9xl"
+      >
+        dark
+      </motion.div>
+    </motion.div>
   );
 };
 
